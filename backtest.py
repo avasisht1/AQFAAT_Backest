101b05accd611558c0018ff05f1fedca36333bc4
avasisht1 <avasisht@andrew.cmu.edu>
2024-02-12 21:06:37 -0500

    Added docstrings to new functions

--- backtest.py
+++ backtest.py
@@ -353,6 +353,22 @@ def run_test(df, name, init_capital=1000, plot_ohlc_rsi=False, plot_equity=False
 opt_universe = {"low_pd":[3,6,2], "rsi_pd":[2,5,2], "rsi_threshold":[40,61,20], "max_dim":[5,6,1]}
 
 def create_table(univ):
+    """
+    Creates a table of all the possible hyperparameter values.
+    
+    Given a dictionary with the hyperparameter names and an indexable object 
+    containing 3 numbers, generates a table where each row is one combination of
+    the hyperparameters. The numbers are interpreted int he same way as in a 
+    slice or range object with the interval [start, end) being the first two, 
+    and the third being the increment.
+
+    Parameters:
+        - univ (dict): The dictionary
+
+    Returns:
+        pd.DataFrame: Dataframe with one row per combination of hyperparameter values
+    """
+    
     param_ranges = [range(*univ[x]) for x in univ]
     combos = []
     for combo in itertools.product(*param_ranges):
@@ -362,6 +378,25 @@ def create_table(univ):
     return values
 
 def optimize_strat(df, values, init_capital=1000):
+    """
+    Finds the best set of hyperparameter values as well as statistics about the
+    various combinations
+    
+    Given a dataframe with historical data, a table of values, and optionally an
+    amount of initial capital, implements the strategy from apply_strat with
+    all the combinations of the hyperparameters, updates the values dataframe
+    with relevant statistics, and prints the best combination to the console.
+
+    Parameters:
+        - df (pd.DataFrame): The dataframe of historical data
+        - values (pd.DataFrame): The values table
+        - [init_capital] (float): The starting capital for the strategy
+
+    Returns:
+        pd.DataFrame: Dataframe with one row per combination of hyperparameter,
+        now with relevant statistics
+    """
+    
     n = values.shape[0]
     names = values.columns
     values['Days in Market'] = [0 for i in range(n)]
